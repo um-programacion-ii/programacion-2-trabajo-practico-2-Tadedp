fba2db07d6e5a4530c7867f44dec30692860b35a
Tadedp <tadeodp22@gmail.com>
2025-04-23 12:11:22 -0300

    Clases concretas desde RecursoDigital implementadas.

---
+++ src/modelos/Libro.java
@@ -0,0 +1,84 @@
+package src.modelos;
+
+import src.enums.EstadoRecurso;
+import src.interfaces.Prestable;
+import src.interfaces.Renovable;
+
+public class Libro extends RecursoDigital implements Prestable, Renovable {
+    private int paginas;
+    private String genero;
+    private long vecesPrestado;
+    private boolean disponible;
+    private boolean renovable;
+
+    public Libro(long id, String titulo, String autor) {
+        super(id, titulo, autor);
+        this.vecesPrestado = 0;
+        this.disponible = true;
+        this.renovable = true;
+    }
+
+    public Libro(long id, String titulo, String autor, int paginas, String genero) {
+        super(id, titulo, autor);
+        this.paginas = paginas;
+        this.genero = genero;
+        this.vecesPrestado = 0;
+        this.disponible = true;
+        this.renovable = true;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    @Override
+    public long getVecesPrestado() {
+        return vecesPrestado;
+    }
+
+    @Override
+    public void prestar() {
+        this.disponible = false;
+        vecesPrestado += 1;
+        this.renovable = true;
+    }
+
+    @Override
+    public void devolver() {
+        this.disponible = true;
+    }
+
+    @Override
+    public boolean puedePrestarse() {
+        return disponible;
+    }
+
+    @Override
+    public void renovar() {
+        this.renovable = false;
+    }
+
+    @Override
+    public boolean puedeRenovarse() {
+        return renovable;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "id=" + this.getId() +
+                ", titulo='" + this.getTitulo() + '\'' +
+                ", autor='" + this.getAutor() + '\'' +
+                ", estado=" + this.getEstadoRecurso() +
+                ", paginas=" + paginas +
+                ", genero='" + genero + '\'' +
+                ", vecesPrestado=" + vecesPrestado +
+                ", disponible=" + disponible +
+                ", renovable=" + renovable +
+                '}';
+    }
+}
